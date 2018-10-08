71c67d5665dcbb0ad6a52b1019370ced62d6a335
Daniel <dfdazac@gmail.com>
2018-10-08 11:11:03 +0200

    Initial commit, add data downloading script

---
+++ get-data.sh
@@ -0,0 +1,8 @@
+# Get original zip file
+wget http://www.cs.uic.edu/~liub/FBS/CustomerReviewData.zip
+unzip CustomerReviewData.zip
+# Move into data folder
+mv "customer review data" raw
+mv raw data/
+# Remove downloaded zip
+rm CustomerReviewData.zip

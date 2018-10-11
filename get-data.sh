6f27bfacd475f0290e3323b82b11c5aa1e56e851
Daniel <dfdazac@gmail.com>
2018-10-11 17:16:00 +0200

    Add embeddings to script

--- get-data.sh
+++ get-data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Get original zip file
+echo "Downloading data..."
 wget http://www.cs.uic.edu/~liub/FBS/CustomerReviewData.zip
 unzip CustomerReviewData.zip
 # Move into data folder
@@ -7,3 +8,9 @@ mv "customer review data" raw
 mv raw data/
 # Remove downloaded zip
 rm CustomerReviewData.zip
+
+# Download GloVe embeddings
+echo "Downloading embeddings..."
+wget http://nlp.stanford.edu/data/glove.6B.zip
+unzip glove.6B.zip -d embeddings
+rm glove.6B.zip

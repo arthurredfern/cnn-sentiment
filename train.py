1f9ab0b4ea39faab206228630858923c31122469
Daniel <dfdazac@gmail.com>
2018-10-15 18:32:24 +0200

    Save model when finished training

--- train.py
+++ train.py
@@ -7,18 +7,43 @@ import numpy as np
 from data import make_dataset
 from model import make_cnn_classifier
 
+def eval_accuracy(sess, accuracy, iterator_init):
+    """Evaluate the accuracy tensor for a particular initialization of the
+    Dataset iterator.
+    Args:
+        sess: a session to evaluate the tensor
+        accuracy: the tensor to evaluate
+        iterator_init: an iterator initializer
+    Returns: float, the calculated accuracy
+    """
+    sess.run(iterator_init)
+    avg_accuracy = 0
+    count = 0
+    try:
+        while True:
+            acc = sess.run(accuracy)
+            avg_accuracy += acc
+            count += 1
+    except tf.errors.OutOfRangeError:
+        pass
+    avg_accuracy /= count
+    return avg_accuracy
+
 def main():
     now = datetime.now().strftime('%Y-%m-%d-%H%M%S')
 
     with tf.name_scope('data'):
         train_data = make_dataset('data/clean/train.tfrecord', batch_size=64)
         dev_data = make_dataset('data/clean/dev.tfrecord', batch_size=128)
+        test_data = make_dataset('data/clean/test.tfrecord', batch_size=128)
 
         iterator = tf.data.Iterator.from_structure(train_data.output_types,
                                                    train_data.output_shapes)
         lengths, labels, words = iterator.get_next()
+        words = tf.identity(words, name='words')
         train_iterator_init = iterator.make_initializer(train_data)
         dev_iterator_init = iterator.make_initializer(dev_data)
+        test_iterator_init = iterator.make_initializer(test_data)
 
     # Model
     #emb_array = np.zeros((400002, 10))
@@ -35,6 +60,7 @@ def main():
 
     with tf.name_scope('accuracy'):
         prediction = tf.nn.sigmoid(logits) > 0.5
+        prediction = tf.identity(prediction, name='prediction')
         correct = tf.equal(tf.cast(prediction, tf.int32), tf.cast(labels, tf.int32))
         accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 
@@ -42,11 +68,13 @@ def main():
     writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
     saver = tf.train.Saver()
 
-    n_epochs = 10
+    n_epochs = 20
     steps = 0
     best_dev_accuracy = 0
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
+
+        # Training loop
         for epoch in range(1, n_epochs + 1):
             print('Epoch {:d}'.format(epoch))
             sess.run(train_iterator_init)
@@ -62,17 +90,8 @@ def main():
             except tf.errors.OutOfRangeError:
                 pass
 
-            sess.run(dev_iterator_init)
-            avg_accuracy = 0
-            count = 0
-            try:
-                while True:
-                    acc = sess.run(accuracy)
-                    avg_accuracy += acc
-                    count += 1
-            except tf.errors.OutOfRangeError:
-                pass
-            avg_accuracy /= count
+            # Evaluate on dev set
+            avg_accuracy = eval_accuracy(sess, accuracy, dev_iterator_init)
             print('\nDev accuracy: {:.4f}'.format(avg_accuracy))
             acc_summ = tf.Summary()
             acc_summ.value.add(tag='dev/accuracy', simple_value=avg_accuracy)
@@ -84,6 +103,22 @@ def main():
                 saver.save(sess, os.path.join(logdir, 'ckpt'), global_step=epoch)
 
     writer.close()
+
+    # Training finished, evaluate on test set and export
+    print('Testing and exporting...')
+    with tf.Session() as sess:
+        saver.restore(sess, tf.train.latest_checkpoint(logdir))
+        avg_accuracy = eval_accuracy(sess, accuracy, test_iterator_init)
+        print('Test accuracy: {:.4f}'.format(avg_accuracy))
+
+        # Export best model as SavedModel
+        export_dir = os.path.join(logdir, 'saved_model')
+        builder = tf.saved_model.builder.SavedModelBuilder(export_dir)
+        builder.add_meta_graph_and_variables(sess,
+            [tf.saved_model.tag_constants.SERVING],
+            main_op=tf.tables_initializer())
+        builder.save()
+
 
 if __name__ == '__main__':
     main()

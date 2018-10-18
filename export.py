9697ed6a93702ab0e21248eb48110eaec97651a8
Daniel <dfdazac@gmail.com>
2018-10-18 20:37:47 +0200

    Handle checkpoints with command line arg

--- export.py
+++ export.py
@@ -1,53 +1,72 @@
 import os
+import argparse
 import tensorflow as tf
 import pickle
 
 from data import UNK
 
-# TODO: Handle with args
+SERVABLES_PATH = './servables/'
 
-# Load mapping from words (str) to IDs (int)
-word_to_id = pickle.load(open('embeddings/glove.6B.300d.wti', 'rb'))
-id_to_word = pickle.load(open('embeddings/glove.6B.300d.itw', 'rb'))
-mapping = tf.constant([id_to_word[id] for id in range(len(id_to_word))])
-table = tf.contrib.lookup.index_table_from_tensor(mapping,
-                                                default_value=word_to_id[UNK])
+def get_new_version():
 
-# Read and split words
-input_words = tf.placeholder(tf.string, name='input_words')
-split_words = tf.string_split(input_words).values
-# Convert input strings to word IDs in mini-batch of size 1
-word_ids = tf.expand_dims(table.lookup(split_words), axis=0)
 
-checkpoint = tf.train.latest_checkpoint('./runs/2018-10-16-201207/')
-# Load graph mapping input tensor
-saver = tf.train.import_meta_graph(checkpoint + '.meta', input_map={'data/words:0': word_ids})
-prediction = tf.get_default_graph().get_tensor_by_name('accuracy/prediction:0')
+def export_ckpt(ckpt_path):
+    """Exports a SavedModel to the servables folder using the checkpoint at the
+    specified path.
+    """
+    # Load mapping from words (str) to IDs (int)
+    word_to_id = pickle.load(open('embeddings/glove.6B.300d.wti', 'rb'))
+    id_to_word = pickle.load(open('embeddings/glove.6B.300d.itw', 'rb'))
+    mapping = tf.constant([id_to_word[id] for id in range(len(id_to_word))])
+    table = tf.contrib.lookup.index_table_from_tensor(mapping,
+                                                    default_value=word_to_id[UNK])
 
-with tf.Session() as sess:
-    tf.tables_initializer().run()
-    saver.restore(sess, checkpoint)
+    # Read and split words
+    input_words = tf.placeholder(tf.string, name='input_words')
+    split_words = tf.string_split(input_words).values
+    # Convert input strings to word IDs in mini-batch of size 1
+    word_ids = tf.expand_dims(table.lookup(split_words), axis=0)
 
-    # Export signatures
-    words_tensor_info = tf.saved_model.utils.build_tensor_info(input_words)
-    preds_tensor_info = tf.saved_model.utils.build_tensor_info(prediction)
-    split_tensor_info = tf.saved_model.utils.build_tensor_info(split_words)
-    ids_tensor_info = tf.saved_model.utils.build_tensor_info(word_ids)
-    prediction_signature = (
-       tf.saved_model.signature_def_utils.build_signature_def(
-           inputs={'input_words': words_tensor_info},
-           outputs={'prediction': preds_tensor_info},
-           method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME))
+    checkpoint = tf.train.latest_checkpoint(ckpt_path)
+    # Load graph mapping input tensor
+    saver = tf.train.import_meta_graph(checkpoint + '.meta', input_map={'data/words:0': word_ids})
+    prediction = tf.get_default_graph().get_tensor_by_name('accuracy/prediction:0')
 
-    # Export model for serving
-    export_dir = os.path.join('servables', '1')
-    builder = tf.saved_model.builder.SavedModelBuilder(export_dir)
-    builder.add_meta_graph_and_variables(
-       sess,
-       [tf.saved_model.tag_constants.SERVING],
-       signature_def_map={
-           tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY: prediction_signature
-       },
-       main_op=tf.tables_initializer()
-    )
-    builder.save()
+    with tf.Session() as sess:
+        tf.tables_initializer().run()
+        saver.restore(sess, checkpoint)
+
+        # Export signatures
+        words_tensor_info = tf.saved_model.utils.build_tensor_info(input_words)
+        preds_tensor_info = tf.saved_model.utils.build_tensor_info(prediction)
+        split_tensor_info = tf.saved_model.utils.build_tensor_info(split_words)
+        ids_tensor_info = tf.saved_model.utils.build_tensor_info(word_ids)
+        prediction_signature = (
+           tf.saved_model.signature_def_utils.build_signature_def(
+               inputs={'input_words': words_tensor_info},
+               outputs={'prediction': preds_tensor_info},
+               method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME))
+
+        # Export model for serving
+        export_dir = os.path.join('servables', '2')
+        builder = tf.saved_model.builder.SavedModelBuilder(export_dir)
+        builder.add_meta_graph_and_variables(
+           sess,
+           [tf.saved_model.tag_constants.SERVING],
+           signature_def_map={
+               tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY: prediction_signature
+           },
+           main_op=tf.tables_initializer()
+        )
+        builder.save()
+
+if __name__ == '__main__':
+    # Read the checkpoint folder from the command line
+    description = 'Export model checkpoint to SavedModel for serving'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('ckpt_path', help='Folder containing checkpoints.'
+        'The last checkpoint is used to load the model.')
+    args = vars(parser.parse_args())
+    ckpt_path = args['ckpt_path']
+
+    export_ckpt(ckpt_path)
